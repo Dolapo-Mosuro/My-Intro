00647b026463cd7905416afa75650482ecdfda8a
Dolapo-Mosuro <mosurodolapo@gmail.com>
2022-06-24 00:43:34 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,5 @@
+let myName = "Dolapo Mosuro";
+let myHeight = "165.1 cm";
+let myCountry = "Nigeria";
+
+alert("my name is " + myName + "\n I am " +myHeight + "tall " + "\n I am a citizen " + myCountry)
