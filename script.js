cc71eef96b0698863fab7510f68aa50e80adb408
Dolapo-Mosuro <mosurodolapo@gmail.com>
2022-06-24 18:15:29 +0100

    update

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ let myHeight = "165.1 cm";
 let myCountry = "Nigeria";
 
 alert("my name is " + myName + "\n I am " +myHeight + "tall " + "\n I am a citizen " + myCountry)
-console.log(myName)
-console.log(myHeight)
-console.log(myCountry)
+//console.log(myName)
+//console.log(myHeight)
+//console.log(myCountry)
