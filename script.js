55b075ef8ce534502de74422f6a165da1dfbf0a5
Dolapo-Mosuro <mosurodolapo@gmail.com>
2022-06-24 18:03:19 +0100

    Commit new code

--- script.js
+++ script.js
@@ -2,4 +2,7 @@ let myName = "Dolapo Mosuro";
 let myHeight = "165.1 cm";
 let myCountry = "Nigeria";
 
-alert("my name is " + myName + "\n I am " +myHeight + "tall " + "\n I am a citizen " + myCountry)
+//alert("my name is " + myName + "\n I am " +myHeight + "tall " + "\n I am a citizen " + myCountry)
+console.log(myName)
+console.log(myHeight)
+console.log(myCountry)
